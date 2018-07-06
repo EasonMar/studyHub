07719504d2bc8197f08c736b39ca024b5c2b2509
zhongyucheng <zhongyucheng@xiangrikui.com>
2018-07-06 16:00:13 +0800

    webpack.vq --- resolve.alias   resolve.extensions

--- webpack/webpack1/webpack.config.js
+++ webpack/webpack1/webpack.config.js
@@ -2,7 +2,8 @@ var webpack = require('webpack');
 var htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
 	//页面入口文件配置
-    entry: __dirname + '/src/js/app.js',
+    // entry: __dirname + '/src/js/app.js',
+    entry:'javascript/app.js', // 配置文件也可以用别名？
     //入口文件输出配置
     output: {
         path: __dirname +'/dist/',
@@ -14,5 +15,11 @@ module.exports = {
             filename: 'index.html',
             template: 'index.html'
         })
-    ]
+    ],
+    resolve: {
+        extensions: ['', '.js', '.css'], // 对应扩展名的资源都可以不用写后缀了
+        alias: {
+            javascript: __dirname +'/src/js'
+        }
+    }
 }
