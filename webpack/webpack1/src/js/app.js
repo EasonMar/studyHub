4777b0ca73c63dbea196d5c689d582034366c554
zhongyucheng <zhongyucheng@xiangrikui.com>
2018-07-06 16:30:01 +0800

    webpack.v1 --- style-loader!css-loader --- upgrade：resolve.alias

--- webpack/webpack1/src/js/app.js
+++ webpack/webpack1/src/js/app.js
@@ -1,5 +1,5 @@
 // import init from './index.js';  // // ES6 Module语法要另外配置
-
-var init = require('./index');
+require('style/index');
+var init = require('javascript/index');
 
 init.fn();
--- webpack/webpack1/webpack.config.js
+++ webpack/webpack1/webpack.config.js
@@ -1,5 +1,5 @@
-var webpack = require('webpack');
-var htmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const htmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
 	//页面入口文件配置
     // entry: __dirname + '/src/js/app.js',
@@ -19,7 +19,14 @@ module.exports = {
     resolve: {
         extensions: ['', '.js', '.css'], // 对应扩展名的资源都可以不用写后缀了
         alias: {
-            javascript: __dirname +'/src/js'
+            javascript: __dirname +'/src/js',
+            style: __dirname+'/src/css'
         }
-    }
+    },
+    module: {
+        //加载器配置
+        loaders: [
+            { test: /[.]css$/, loader: 'style-loader!css-loader' }  // '!'表示级联调用
+        ]
+    },
 }
