8d70e231850b2aac51315115959e438d6283eb3f
EasonMar <12054648@qq.com>
2018-08-12 17:59:14 +0800

    feature：配置容错 & 输出错误Log feature：增加Mock Model -- > indexModel.js，并在indexController.js中引入

--- Architect/lesson_one/src/nodeuii/app.js
+++ Architect/lesson_one/src/nodeuii/app.js
@@ -5,27 +5,35 @@ import router from 'koa-route';
 import initController from './controllers/index';
 
 import swig from 'koa-swig';
-import errorHandler from './middlewares/errorHandler';
 import co from 'co';
-
 import polyfill from 'babel-polyfill';
+import errorHandler from './middlewares/errorHandler';
+// 应用log4js
+const log4js = require('log4js');
+// 配置
+log4js.configure({
+    appenders: { cheese: { type: 'file', filename: __dirname + '/logs/eason.log' } },
+    categories: { default: { appenders: ['cheese'], level: 'error' } }
+});
+const logger = log4js.getLogger('cheese');
+
 const app = new Koa();
 
 // 在初始化路由之前, 配置errorHandler
 // 本质是保证errorHandler定义在所有中间件的最前面, 这样可以确保errorHandler是兜底的操作
-errorHandler.error(app);
+errorHandler.error(app, logger);
 
 initController(app, router);
 
 // co.wrap(), swig的配置需要详细了解
 app.context.render = co.wrap(swig({
-	root: config.viewDir,
-	autoescape: true, // 改变当前变量的自动转义行为
-	cache: false, // disable, set to false 。这里设置模板缓存！
-	ext: 'html',
-	writeBody: false
+    root: config.viewDir,
+    autoescape: true, // 改变当前变量的自动转义行为
+    cache: false, // disable, set to false 。这里设置模板缓存！
+    ext: 'html',
+    writeBody: false
 }));
 
 app.listen(config.port, () => {
-	console.log(`lesson_one is listenning on ${config.port}`)
+    console.log(`lesson_one is listenning on ${config.port}`)
 });
--- Architect/lesson_one/src/nodeuii/controllers/indexController.js
+++ Architect/lesson_one/src/nodeuii/controllers/indexController.js
@@ -1,15 +1,20 @@
+import IndexModel from '../models/IndexModel.js'
 // 路由控制中心！各种具体的页面渲染、内容返回的操作统一放到这里！
 class IndexController {
     constructor() {}
 
     indexAction() {
-        return async (ctx, next) => {
-            ctx.body = await ctx.render('index.html');
+        return async(ctx, next) => {
+            const IndexModelIns = new IndexModel();
+            const result = await IndexModelIns.getData();
+            ctx.body = await ctx.render('index.html', {
+                data: result
+            });
         }
     }
 
     interface() {
-        return async (ctx, next) => {
+        return async(ctx, next) => {
             ctx.body = { code: 200 }
         }
     }
