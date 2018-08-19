44408b612f7b5c482c97afc70d5b83d79f76e681
EasonMar <12054648@qq.com>
2018-08-19 10:43:54 +0800

    awilix-koa complete!

--- Architect/Notes/usage_awilix-koa.js
+++ Architect/Notes/usage_awilix-koa.js
@@ -45,3 +45,83 @@ const api = makeInvoker(makeAPI)
 // Creates middleware that will invoke `makeAPI`
 // for each request, giving you a scoped instance.
 router.get('/todos', api('find'))
+
+
+
+
+/**
+ * Awesome Usage
+ *
+ * As of awilix-koa@1.0.0, we ship with koa-router bindings for awilix-router-core! 
+ * This is cool because now your routing setup can be streamlined with first-class Awilix support!
+ *
+ * The Awilix-based router comes in 2 flavors-风格: a builder and ESNext decorators.
+ *
+ * Please see the 'awilix-router-core' docs for information about the full API.
+ */
+
+// routes/todos-api.js - demos the builder pattern
+import bodyParser from 'koa-bodyparser'
+import { authenticate } from './your-auth-middleware'
+import { createController } from 'awilix-koa' // or `awilix-router-core`
+
+const API = ({ todoService }) => ({
+    getTodo: async ctx => (ctx.body = await todoService.get(ctx.params.id)),
+    createTodo: async ctx =>
+        (ctx.body = await todoService.create(ctx.request.body))
+})
+
+export default createController(API)
+    .prefix('/todos') // Prefix all endpoints with `/todo`
+    .before([authenticate()]) // run authentication for all endpoints
+    .get('/:id', 'getTodo') // Maps `GET /todos/:id` to the `getTodo` function on the returned object from `API`
+    .post('', 'createTodo', {
+        // Maps `POST /todos` to the `createTodo` function on the returned object from `API`
+        before: [bodyParser()] // Runs the bodyParser just for this endpoint
+    })
+
+
+// routes/users-api.js - demos the decorator pattern
+import bodyParser from 'koa-bodyparser'
+import { authenticate } from './your-auth-middleware'
+import { route, GET, POST, before } from 'awilix-koa' // or `awilix-router-core`
+
+@route('/users')
+export default class UserAPI {
+    constructor({ userService }) {
+        this.userService = userService
+    }
+
+    @route('/:id')
+    @GET()
+    @before([authenticate()])
+    async getUser(ctx) {
+        ctx.body = await this.userService.get(ctx.params.id)
+    }
+
+    @POST()
+    @before([bodyParser()])
+    async createUser(ctx) {
+        ctx.body = await this.userService.create(ctx.request.body)
+    }
+}
+
+
+// server.js
+import Koa from 'koa'
+import { asClass, createContainer } from 'awilix'
+import { loadControllers, scopePerRequest } from 'awilix-koa'
+
+const app = new Koa()
+const container = createContainer()
+    .register({
+        userService: asClass( /*...*/ ),
+        todoService: asClass( /*...*/ )
+    })
+app.use(scopePerRequest(container))
+// Loads all controllers in the `routes` folder
+// relative to the current working directory.
+// This is a glob pattern.
+app.use(loadControllers('routes/*.js', { cwd: __dirname }))
+
+app.listen(3000)
