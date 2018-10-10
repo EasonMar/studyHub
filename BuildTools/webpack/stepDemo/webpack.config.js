8368bf6dab869b400012a80f71527a73b70ff12e
EasonMar <12054648@qq.com>
2018-10-10 09:43:00 +0800

    webpack demo - asset-management：  load others data

--- BuildTools/webpack/stepDemo/webpack.config.js
+++ BuildTools/webpack/stepDemo/webpack.config.js
@@ -25,11 +25,26 @@ module.exports = {
             /**
              * file-loader 和 url-loader 可以接收并加载任何文件，然后将其输出到构建目录
              * 这就是说，我们可以将它们用于任何类型的文件，包括字体
-             */ 
+             */
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: [
                     'file-loader'
+                ]
+            },
+            /**
+             * 要导入 CSV、TSV 和 XML，你可以使用 csv-loader 和 xml-loader
+             */ 
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
                 ]
             }
         ]
