5d3849ff939f5a9e3d6151c5803756769ba7426a
EasonMar <12054648@qq.com>
2018-10-10 11:37:27 +0800

    webpack demo - output-management：准备工具

--- BuildTools/webpack/stepDemo/webpack.config.js
+++ BuildTools/webpack/stepDemo/webpack.config.js
@@ -5,48 +5,5 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    },
-    // loader在module里面使用
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            /**
-             * file-loader 和 url-loader 可以接收并加载任何文件，然后将其输出到构建目录
-             * 这就是说，我们可以将它们用于任何类型的文件，包括字体
-             */
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            /**
-             * 要导入 CSV、TSV 和 XML，你可以使用 csv-loader 和 xml-loader
-             */ 
-            {
-                test: /\.(csv|tsv)$/,
-                use: [
-                    'csv-loader'
-                ]
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                    'xml-loader'
-                ]
-            }
-        ]
     }
 };
