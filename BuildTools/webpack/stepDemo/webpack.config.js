0ed4d86cf98c094bc94c09344e702362f9ac726c
EasonMar <12054648@qq.com>
2018-10-15 11:31:22 +0800

    webpack demo - code-splitting：via entry

--- BuildTools/webpack/stepDemo/webpack.config.js
+++ BuildTools/webpack/stepDemo/webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    mode: 'development',
+    entry: { 
+        index: './src/index.js', 
+        another: './src/another-module.js'
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     }
 };
