78fd59bf7e87168fa86bf8164666cb00f0b61ce1
EasonMar <12054648@qq.com>
2018-12-06 14:55:17 +0800

    Data Structure and Algorithms ：quickSort

--- DataStructureAndAlgorithms/Sort/mergeSort.js
+++ DataStructureAndAlgorithms/Sort/mergeSort.js
@@ -1,11 +1,54 @@
-// 递推公式：merge_sort(p…r) = merge(merge_sort(p…q), merge_sort(q+1…r))
-// 终止条件：p >= r 不用再继续分解
+/**
+ * 归并排序的核心思想还是蛮简单的。如果要排序一个数组, 我们先把数组从中间分成前后两部分, 
+ * 然后对前后两部分分别排序, 再将排好序的两部分合并在一起, 这样整个数组就都有序了
+ * 注意：这个"合并"本身也具有排序的功能在内,才能保证合并之后整个数组是有序的
+ */
 
-
+/** 
+ * 递推公式：merge_sort(p…r) = merge(merge_sort(p…q), merge_sort(q+1…r))
+ * 终止条件：p >= r 不用再继续分解
+ */
 
 /**
+ * 归并排序算法
+ * @param  {Array}  A  待排序的数组
+ * @param  {number} n  数组大小
+ */
+function merge_sort(A, n) {
+    merge_sort_c(A, 0, n - 1)
+}
+
+/**
+ * 递归调用函数
+ * @param  {Array}  A  待排序的数组
+ * @param  {number} p  起始位置
+ * @param  {[type]} r  结束位置
+ */
+function merge_sort_c(A, p, r) {
+    // 递归终止条件
+    if (p >= r) return;
+    // console.log('merge_sort_c');
+    // console.log([p,r]);
+
+    // 取 p 到 r 之间的中间位置 q
+    // q = Math.floor((p + r) / 2); q成全局变量了, 影响了数组的拆分！！
+    let q = Math.floor((p + r) / 2);
+
+    // 分治递归
+    merge_sort_c(A, p, q)
+    merge_sort_c(A, q + 1, r)
+    // 将 A[p...q] 和 A[q+1...r] 合并为 A[p...r]
+    // JS如何实现数组分片？
+    // 因为实际上并未使用传入的分片, 所以只需要传入原数组A和分片区域即可
+    merge(A, [p, q], [q + 1, r])
+}
+
+/**
+ * 你可能已经发现了, merge(A[p…r], A[p…q], A[q+1…r]) 这个函数的作用就是, 
+ * 将已经有序的 A[p…q] 和 A[q+1…r] 合并成一个有序的数组, 并且放入 A[p…r]
+ * 
  * merge函数是关键
- * 将已经有序的 left_Array 和 right_Array 合并成一个有序的数组
+ * 将已经有序的 left_Array 和 right_Array 合并成一个整体有序的数组
  * @param  {Array} A   原数组
  * @param  {Array} L   左半数组区间:[start,end]
  * @param  {Array} R   右半数组区间:[start,end]
@@ -14,15 +57,13 @@ function merge(A, L, R) {
 	console.log('merge info：');
     console.log([L.toString(),R.toString()]);
 
-	// 初始化变量 i, j, k, q, r
+	// 初始化局部变量
     var i = p = L[0], // i = 排序初始位置 = 左半数组 初始位置
         j = R[0],	  // j = 右半数组 初始位置
-        k = 0,
         q = L[1], 	  // q = 左半数组 结束位置	
-        r = R[1];     // r = 右半数组 结束位置
-
-
-    var tmp = new Array(A.length); // 申请一个大小跟 A 一样的临时数组
+        r = R[1],     // r = 右半数组 结束位置
+        k = 0,        // 临时数组指针
+        tmp = new Array(A.length); // 申请一个大小跟 A 一样的临时数组
 
     /**
      * 用两个游标 i 和 j, 分别指向 A[p…q] 和 A[q+1…r] 的第一个元素
@@ -56,43 +97,9 @@ function merge(A, L, R) {
     }
 }
 
+// 出现过的问题：只能实现最大数的冒泡...
+// 拆分有问题：因为变量q不是局部的,导致拆分出问题！
 
-
-/**
- * 归并排序算法
- * @param  {Array}  A  待排序的数组
- * @param  {number} n  数组大小
- */
-function merge_sort(A, n) {
-    merge_sort_c(A, 0, n - 1)
-}
-
-/**
- * 递归调用函数
- * @param  {Array}  A  待排序的数组
- * @param  {number} p  起始位置
- * @param  {[type]} r  结束位置
- */
-function merge_sort_c(A, p, r) {
-    // 递归终止条件
-    if (p >= r) return;
-    // console.log('merge_sort_c');
-    // console.log([p,r]);
-
-    // 取 p 到 r 之间的中间位置 q
-    // q = Math.floor((p + r) / 2); q成全局变量了, 影响了数组拆分！！
-    let q = Math.floor((p + r) / 2);
-
-    // 分治递归
-    merge_sort_c(A, p, q)
-    merge_sort_c(A, q + 1, r)
-    // 将 A[p...q] 和 A[q+1...r] 合并为 A[p...r]
-    // JS如何实现数组分片？
-    // 因为实际上并未使用传入的分片, 所以只需要传入原数组A和分片区域即可
-    merge(A, [p, q], [q + 1, r])
-}
-
-// 目前的实现有问题, 只能实现最大数的冒泡...(拆分有问题：因为变量q不是局部的,导致拆分出问题！)
 // var a = [8,7,6,9,5,10,1,3]; // 8个元素,可以一直两两平分
 // merge_sort(a, 8);
 // var a = [8,7,6,9,5,10]; // 6个元素,拆分一次就变成单数个
---
+++ DataStructureAndAlgorithms/Sort/quickSort.js
@@ -0,0 +1,83 @@
+/** 
+ * 快排的思想是这样的：
+ * 
+ * 如果要排序数组中下标从 p 到 r 之间的一组数据, 
+ * 我们选择 p 到 r 之间的任意一个数据作为pivot(分区点)
+ * 
+ * 我们遍历 p 到 r 之间的数据, 将小于 pivot 的放到左边, 将大于 pivot 的放到右边, 将 pivot 放到中间。
+ * 经过这一步骤之后, 数组 p 到 r 之间的数据就被分成了三个部分, 
+ * 前面 p 到 q-1 之间都是小于 pivot 的, 中间是 pivot, 后面的 q+1 到 r 之间是大于 pivot 的
+ * 
+ * 根据分治、递归的处理思想, 我们可以用递归排序下标从 p 到 q-1 之间的数据和下标从 q+1 到 r 之间的数据, 
+ * 直到区间缩小为 1, 就说明所有的数据都有序了
+ */
+
+
+/**
+ * 递推公式：quick_sort(p…r) = quick_sort(p…q-1) + quick_sort(q+1, r)
+ * 终止条件：p >= r
+ */
+
+// 快速排序, A 是数组, n 表示数组的大小
+function quick_sort(A, n) {
+    quick_sort_c(A, 0, n - 1)
+}
+// 快速排序递归函数, p,r 为下标(排序区间的左右边界下标)
+function quick_sort_c(A, p, r) {
+    console.log('quick_sort_c');
+    console.log([p, r]);
+    if (p >= r) return;
+
+    let q = partition(A, p, r) // 获取分区点
+    quick_sort_c(A, p, q - 1)
+    quick_sort_c(A, q + 1, r)
+}
+
+/**
+ * 归并排序中有一个 merge() 合并函数, 我们这里有一个 partition() 分区函数
+ * partition() 分区函数实际上我们前面已经讲过了, 
+ * 就是随机选择一个元素作为 pivot(一般情况下, 可以选择 p 到 r 区间的最后一个元素), 
+ * 然后对 A[p…r] 分区, 函数返回 pivot 的下标
+ * 
+ * 
+ * 如果我们不考虑空间消耗的话, partition() 分区函数可以写得非常简单
+ * 我们申请两个临时数组 X 和 Y, 遍历 A[p…r], 将小于 pivot 的元素都拷贝到临时数组 X, 
+ * 将大于 pivot 的元素都拷贝到临时数组 Y, 最后再将数组 X 和数组 Y 中数据顺序拷贝到 A[p…r]
+ * 
+ * 但是, 如果按照这种思路实现的话, partition() 函数就需要很多额外的内存空间, 所以快排就不是原地排序算法了
+ * 如果我们希望快排是原地排序算法, 那它的空间复杂度得是 O(1), 
+ * 那 partition() 分区函数就不能占用太多额外的内存空间, 我们就需要在 A[p…r] 的原地完成分区操作
+ * 
+ * 原地分区函数的实现思路非常巧妙
+ */
+
+/**
+ * 分区函数
+ * @param {Array} A 原数组
+ * @param {Number} p 排序区间 左边界
+ * @param {Number} r 排序区间 右边界
+ */
+function partition(A, p, r) {
+    let pivot = A[r], i = p;
+    for (let j = p; j <= r - 1; j++) {
+        if (A[j] < pivot) {
+            // swap A[i] with A[j] // 交换,js如何方便的实现数组交换？splice、解构赋值(无需第三个变量的情况下实现2个变量数值交换)？
+            [A[i], A[j]] = [A[j], A[i]]; // 使用解构赋值进行数值交换
+            i++; // 需要理解好i指针的意义：指向比pivot大的数,并等待跟后面的比pivot小的数交换位置
+        }
+        console.log('[i,j,pivot]');
+        console.log([i, j, pivot]);
+    }
+    // swap A[i] with A[r]
+    // 遍历完后, i指向的是比pivot大的最左边的数, 且i左边的数都比pivto小
+    // A[i]跟pivot对调, 就完成了pivot左边的数比pivot小, 右边的数比pivot大的分区
+    [A[i], A[r]] = [A[r], A[i]];
+    return i
+}
+
+// var a = [8,7,6,9,5,10,1,3]; // 8个元素,可以一直两两平分
+// quick_sort(a, 8);
+// var a = [8,7,6,9,5,10]; // 6个元素,拆分一次就变成单数个
+// quick_sort(a, 6);
+var a = [8, 7, 6, 9, 5]; // 5个元素,从一开始就是但数个
+quick_sort(a, 5);
