71d7db1ddd854bfb3034d742cfaf266849f7992d
EasonMar <12054648@qq.com>
2019-03-31 20:10:49 +0800

    Do some testing of Vue components

--- Framework/VueDemo/vue_2.0/simplecli2.x/src/main.js
+++ Framework/VueDemo/vue_2.0/simplecli2.x/src/main.js
@@ -1,9 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 注册全局组件
+Vue.component('GlobalComponent',{
+  props:['content'],
+  template:`<h1>{{ content }}</h1>`
+})
+
 new Vue({
   el: '#app',
-  // vue会找到index.html中的 #app 元素，然后用 App组件 替换掉它，渲染到浏览器中 
+  // vue会找到index.html中的 #app 元素，然后用 App组件 替换掉它，渲染到浏览器中
+
   // 了解清楚 render 函数的作用 - 这里 h参数 实际上传入的是 createElement 
-  render: h => h(App)
+  // render: h => h(App)
+
+  // 还有下面这种常见的渲染方式
+  components:{ App },
+  template:'<App/>'
 })
