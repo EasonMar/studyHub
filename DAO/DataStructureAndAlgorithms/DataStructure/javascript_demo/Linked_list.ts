fdd3316fd4472a3cc8f391da8918d79c8af35d3d
EasonMar <12054648@qq.com>
2019-04-20 11:06:17 +0800

    双向链表实现～ 也不太容易啊～ 蛮多坑的

--- DAO/DataStructureAndAlgorithms/DataStructure/javascript_demo/Linked_list.ts
+++ DAO/DataStructureAndAlgorithms/DataStructure/javascript_demo/Linked_list.ts
@@ -1,7 +1,10 @@
 +function () {
     // LRU 缓存淘汰算法实现
 
-    // 节点类
+    // 应该要针对链表的哨兵/头节点 单独设置一个类。
+    // 但是在Typescript中这样做会遇到很多类型不确定报错的问题！！
+
+    // 单向节点
     class Node {
         // 联合类型（Union Types）：表示取值可以为多种类型中的一种
         data: string | null;
@@ -12,8 +15,9 @@
         }
     }
 
+    // 单向链表
     class Linked_list {
-        head: Node;
+        head: Node; // 带头链表：不知不觉就用了哨兵技巧
         maxlen: number;
         constructor(_maxlen: number) {
             this.head = new Node();
@@ -145,8 +149,247 @@
     // a.length()
     // a.moveToTop('111')
     // a.display()
-    
-    let a = new Linked_list(5)
+
+    // let a = new Linked_list(5)
+    // a.cache('111')
+    // a.display()
+    // a.cache('222')
+    // a.display()
+    // a.cache('333')
+    // a.display()
+    // a.cache('111')
+    // a.display()
+    // a.cache('444')
+    // a.display()
+    // a.cache('555')
+    // a.display()
+    // a.cache('333')
+    // a.display()
+    // a.cache('666')
+    // a.display();
+
+
+    // 双向节点
+    class DuNode {
+        data: string | null;
+        next: DuNode | null;
+        prev: DuNode | null;
+        constructor(_data: string | null = null) {
+            this.data = _data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+    // 双向链表
+    class DuLinkedList {
+        head: DuNode;
+        maxlen: number;
+        constructor(_maxlen: number) {
+            this.maxlen = _maxlen;
+            this.head = new DuNode();
+        }
+
+        // 在链表尾部添加节点
+        append(_data: string): void {
+            console.log(`append DuNode ${_data}`);
+            let theNew = new DuNode(_data);
+            let cur = this.head;
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            cur.next = theNew;
+
+            // 处理prev - next 自带为 null
+            theNew.prev = cur;
+        }
+
+        // 在链表头部添加节点
+        unshift(_data: string): void {
+            console.log(`unshift DuNode ${_data}`);
+            let theNew = new DuNode(_data);
+            let theHead = this.head;
+            let theNext = this.head.next
+
+            // 处理next
+            theNew.next = theHead.next;
+            theHead.next = theNew;
+
+            // 处理prev
+            theNew.prev = theHead;
+            if (theNext != null) {
+                theNext.prev = theNew;
+            }
+        }
+
+        // 删除尾部节点
+        pop(): void {
+            console.log('do popping');
+            let cur = this.head;
+
+            // 找到最后一个节点
+            while (cur.next != null) {
+                cur = cur.next;
+            }
+            // 要加好多傻逼的条件啊 cur.prev ！= null
+            // 用来证明 cur 不是头节点
+            if (cur.prev) {
+                // 断绝所有联系
+                cur.prev.next = null
+                cur.prev = null
+            }
+        }
+
+        // 将某节点移动道头部
+        moveToTop(_data: string): void {
+            let cur = this.head; // 必须从哨兵开始，如果跳过哨兵，就得附加很多条件
+            let orginFirst = this.head.next;
+            let found = false;
+            while (cur.next != null) {
+                cur = cur.next;
+                if (cur.data == _data) {
+                    found = true;
+                    break;
+                }
+            }
+
+            // 如果找到了
+            if (found) {
+                /* this.head.next = cur;
+
+                if(cur.next){
+                    cur.next.prev = cur.prev
+                }
+
+                // 要加好多傻逼的条件啊 cur.prev ！= null
+                if (cur.prev) {
+                    cur.prev.next = cur.next; // 把原先cur位置给补上
+                    cur.prev = this.head;
+                }
+
+                if (orginFirst) {
+                    orginFirst.prev = cur; // 处理之前首节点的 prev
+                    cur.next = orginFirst; // 如果 orginFirst 不为 null 才进行 cur.next 指向
+                } */
+                this.moveNodeToTop(cur); // 这样功能更细分
+
+                console.log(`move ${_data} to the top`);
+            } else {
+                console.log('没有找到此节点！');
+            }
+        }
+        // 跳过哨兵看看会多了哪些条件
+        moveToTopDemo(_data: string): void {
+            let cur = this.head.next;
+            let orginFirst = this.head.next;
+            let found = false;
+            while (cur != null) {
+                if (cur.data == _data) {
+                    found = true;
+                    break;
+                }
+                cur = cur.next;
+            }
+
+            // 如果找到了 - 要证明 cur != null - 跳过哨兵多了这个条件
+            if (found && cur) {
+                // 处理next
+                this.head.next = cur;
+
+                if(cur.next){
+                    cur.next.prev = cur.prev
+                }
+
+                // 要加好多傻逼的条件啊 cur.prev ！= null
+                if (cur.prev) {
+                    cur.prev.next = cur.next;
+                    cur.prev = this.head;
+                }
+
+                if (orginFirst) {
+                    orginFirst.prev = cur;
+                    cur.next = orginFirst;
+                }
+
+                console.log(`move ${_data} to the top`);
+            } else {
+                console.log('没有找到此节点！');
+            }
+        }
+
+        // 传入节点, 并将节点放到首位
+        moveNodeToTop(node: DuNode): void {
+            let orginFirst = this.head.next;
+            let beforeNode = node.prev;
+            let afterNode = node.next;
+
+            this.head.next = node; // 将"哨兵"的next指向node
+            
+            if(afterNode){
+                afterNode.prev = node.prev // 将afterNode的prev指向node的prev
+            }
+
+            if(beforeNode){
+                beforeNode.next = node.next; // 将beforeNode的next指向node的next
+                node.prev = this.head;      // 将node的prev指向"哨兵"
+            }
+
+            if(orginFirst){
+                orginFirst.prev = node; // 将之前"首节点"的prev指向node
+                node.next = orginFirst  // 将node的next指向之前的"首节点"
+            }
+        }
+
+        // 查找链表中是否存在某数据
+        find(_data: string): DuNode | null{
+            let cur = this.head;
+            while (cur.next != null) {
+                cur = cur.next;
+                if (cur.data == _data) {
+                    return cur;
+                }
+            }
+            return null;
+        }
+
+        // 查询链表长度
+        length(): number {
+            let cur = this.head;
+            let total = 0;
+            while (cur.next != null) {
+                cur = cur.next;
+                total++;
+            }
+            return total;
+        }
+
+        // 通过数组的方式展示
+        display(): Array<string> {
+            let cur = this.head;
+            let temp = [];
+            while (cur.next != null) {
+                cur = cur.next; // 先移动一格，因为表头是没有数据的
+                temp.push(cur); // 保证temp内的元素不为null
+            }
+            console.log(temp); // 临时用作展示
+            return [temp.toString()];
+        }
+
+        // 数据缓存
+        cache(_data: string): void {
+            console.log(`cache control ${_data}`);
+            let findNode = this.find(_data);
+            if (findNode) {
+                this.moveNodeToTop(findNode);
+            } else if (this.length() == this.maxlen) {
+                this.pop();
+                this.unshift(_data);
+            } else {
+                this.unshift(_data);
+            }
+        }
+    }
+
+    let a = new DuLinkedList(5)
     a.cache('111')
     a.display()
     a.cache('222')
