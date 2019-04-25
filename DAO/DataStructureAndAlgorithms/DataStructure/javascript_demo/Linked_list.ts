8e9d85ff72daa6b5a4f69546d106d9788fb69c40
EasonMar <12054648@qq.com>
2019-04-25 17:22:14 +0800

    1. 实现单链表反序 2. 寻找中间结点算法优化

--- DAO/DataStructureAndAlgorithms/DataStructure/javascript_demo/Linked_list.ts
+++ DAO/DataStructureAndAlgorithms/DataStructure/javascript_demo/Linked_list.ts
@@ -1,10 +1,10 @@
 +function () {
     // LRU 缓存淘汰算法实现
 
-    // 应该要针对链表的哨兵/头节点 单独设置一个类。
+    // 应该要针对链表的哨兵/头结点 单独设置一个类。
     // 但是在Typescript中这样做会遇到很多类型不确定报错的问题！！
 
-    // 单向节点
+    // 单向结点
     class Node {
         // 联合类型（Union Types）：表示取值可以为多种类型中的一种
         // 有时候需要将一个联合类型的变量指定为一个更加具体的类型，就要用到 "类型断言"
@@ -25,7 +25,7 @@
             this.maxlen = _maxlen;
         }
 
-        // 在链表尾部添加节点
+        // 在链表尾部添加结点
         append(_data: string): void {
             console.log('append ' + _data);
             let newNode = new Node(_data);
@@ -36,7 +36,7 @@
             cur.next = newNode;
         }
 
-        // 在链表头部添加节点
+        // 在链表头部添加结点
         unshift(_data: string): void {
             console.log('unshift ' + _data);
             let theHead = this.head;
@@ -45,7 +45,7 @@
             theHead.next = newNode;
         }
 
-        // 删除尾部节点
+        // 删除尾部结点
         pop(): void {
             console.log('do popping');
             let cur = this.head;
@@ -57,7 +57,7 @@
             last.next = null;
         }
 
-        // 将某节点移动道头部
+        // 将某结点移动道头部
         moveToTop(_data: string): void {
             let cur = this.head;
             let last = this.head;
@@ -78,7 +78,7 @@
                 this.head.next = cur;
                 console.log(`move ${_data} to the top`);
             } else {
-                console.log('没有找到此节点！');
+                console.log('没有找到此结点！');
             }
         }
 
@@ -175,7 +175,7 @@
     }
 
 
-    // 双向节点
+    // 双向结点
     class DuNode {
         data: string | null;
         next: DuNode | null;
@@ -195,7 +195,7 @@
             this.head = new DuNode();
         }
 
-        // 在链表尾部添加节点
+        // 在链表尾部添加结点
         append(_data: string): void {
             console.log(`append DuNode ${_data}`);
             let theNew = new DuNode(_data);
@@ -209,7 +209,7 @@
             theNew.prev = cur;
         }
 
-        // 在链表头部添加节点
+        // 在链表头部添加结点
         unshift(_data: string): void {
             console.log(`unshift DuNode ${_data}`);
             let theNew = new DuNode(_data);
@@ -227,16 +227,16 @@
             }
         }
 
-        // 删除尾部节点
+        // 删除尾部结点
         pop(): void {
             console.log('do popping');
             let cur = this.head;
 
-            // 找到最后一个节点
+            // 找到最后一个结点
             while (cur.next != null) {
                 cur = cur.next;
             }
-            // 证明链表内存在节点，即 cur 不是 哨兵 this.head
+            // 证明链表内存在结点，即 cur 不是 哨兵 this.head
             if (cur.prev) {
                 // 断绝所有联系
                 cur.prev.next = null
@@ -244,7 +244,7 @@
             }
         }
 
-        // 将某节点移动道头部
+        // 将某结点移动道头部
         moveToTop(_data: string): void {
             let cur = this.head; // 如果跳过哨兵，过程又会有不一样的地方
             let found = false;
@@ -261,7 +261,7 @@
                 this.moveNodeToTop(cur); // 这样功能更细分
                 console.log(`move ${_data} to the top`);
             } else {
-                console.log('没有找到此节点！');
+                console.log('没有找到此结点！');
             }
         }
         // 跳过哨兵看看会有什么不同
@@ -287,27 +287,27 @@
                 this.head.next = found_node;
                 found_node.prev = this.head;
 
-                // 如果cur不是尾节点, 则要处理其后节点的 prev
+                // 如果cur不是尾结点, 则要处理其后结点的 prev
                 if (after_node) {
                     after_node.prev = before_node;
                 }
 
-                // 如果cur不是首节点, 则要处理其前节点的 next
+                // 如果cur不是首结点, 则要处理其前结点的 next
                 if (before_node) {
                     before_node.next = after_node;
                 }
 
-                // 如果找到了node，可以断言orginFirst不为null，首节点交接
+                // 如果找到了node，可以断言orginFirst不为null，首结点交接
                 (<DuNode>orginFirst).prev = found_node;
                 found_node.next = orginFirst;
 
                 console.log(`move ${_data} to the top`);
             } else {
-                console.log('没有找到此节点！');
+                console.log('没有找到此结点！');
             }
         }
 
-        // 传入节点, 并将节点放到首位
+        // 传入结点, 并将结点放到首位
         moveNodeToTop(the_node: DuNode): void {
             let orginFirst = this.head.next;
             let beforeNode = the_node.prev;
@@ -324,9 +324,9 @@
                 beforeNode.next = afterNode; // 将 beforeNode 的 next指向 the_node 原先的 next
             }
 
-            // 如果找到了the_node，可以断言orginFirst不为null，首节点交接
-            (<DuNode>orginFirst).prev = the_node; // 将之前"首节点"的prev指向the_node
-            the_node.next = orginFirst  // 将the_node的next指向之前的"首节点"
+            // 如果找到了the_node，可以断言orginFirst不为null，首结点交接
+            (<DuNode>orginFirst).prev = the_node; // 将之前"首结点"的prev指向the_node
+            the_node.next = orginFirst  // 将the_node的next指向之前的"首结点"
         }
 
         // 查找链表中是否存在某数据
@@ -399,7 +399,7 @@
         a.display()
     }
 
-    DuLinkedList_test();
+    // DuLinkedList_test();
 
     // 判断字符串是否为回文字符串
     // Given a singly linked list, determine if it is a palindrome.
@@ -410,34 +410,95 @@
 
         // 找到链表中点
         findcenter() {
-            let pointer: Node | null = null; // 慢指针
-            let fast_pointer: Node | null = null; // 快指针
-            let fast_cur: Node | null;
 
-            // 先确定链表长度，必须大于等于2才有回文的可能
-            // 然后看是奇数还是偶数
+            /* let pointer: Node | null = null; // 慢指针
+            let fast_pointer: Node | null = null; // 快指针
+            let first_step: Node | null;
+            // 先确定链表长度，必须大于等于2才有回文的可能 -- 不一定，得看定义，好像长度为1也可以？
+            // 然后看链表长度是奇数还是偶数 - 奇数如何、偶数又如何
             if (this.length() >= 2) {
                 pointer = <Node>this.head.next;
                 // if(pointer){ // 用Typescript经常可能会要求写这种弱智条件！！ -- 可以用断言解决
                 fast_pointer = pointer.next;
                 // }
                 while (fast_pointer != null) {
-                    fast_cur = fast_pointer.next;
-                    if (fast_cur) {
-                        fast_pointer = fast_cur.next
+                    first_step = fast_pointer.next; // "快指针的第一步"
+                    // 如果"快指针的第一步"不为null，第二步为null，则说明此链表为奇数结点链表
+                    if (first_step) {
+                        fast_pointer = first_step.next
                         // if(pointer){ // 用Typescript经常可能会要求写这种弱智条件！！ -- 可以用断言解决
-                        pointer = <Node>pointer.next;
+                        pointer = <Node>pointer.next; // 如果在"快指针的第一步"不为null时才推进慢指针，则在偶数结点链表时，此中点为下位中点
                         // }
+                    } else {
+                        // 如果"快指针的第一步"即为null，说明此链表为偶数结点链表
+                        fast_pointer = null;
                     }
                 }
+                console.log(`中间结点为: ${pointer.data}`);
+            } else {
+                console.log('长度不够')
+            } */
 
+            // 优化：减少变量 和 过程
+            let slow : Node | null = null; 
+            let fast : Node | null = null;
+            let head = this.head;
+            if(head.next == null){
+                console.log(`链表无数据`)
+                return true;
+            }
+            slow = head.next;
+            // fast = head.next; // 在偶数结点链表时，取下位中点
+            fast = head.next.next; // 在偶数结点链表时，取上位中点
+            while(fast != null && fast.next != null){
+                fast = fast.next.next;
+                slow = (<Node>slow).next;
+            }
+
+            console.log(`中间结点为: ${(<Node>slow).data}`)
+        }
+
+        // 单链表反序 - 把后者的指针指向当前 --- 非常考验对"指针"的理解和掌握
+        // 传入的结点之后的结点反序，默认传入哨兵
+        // 双向链表反向应该比较容易
+        reverse(head:Node = this.head) {
+            /* let last: Node | null = null; // 上一个结点
+            let cur = head.next; // 当前结点
+            let iteror = head.next; // 下一个结点（记录遍历顺序）
+            while (iteror != null) {
+                cur = iteror; // 保存当前结点（cur指向新的结点）
+                iteror = cur.next; // 储存下一个结点（这时候iteror指向了另一个结点）
+                cur.next = last; // 改变cur.next指向
+                last = cur; // 使用last指向当前结点（供下一轮使用）
+            }
+            head.next = cur; // 哨兵/传入结点 指向最后的"当前结点" */
+
+            // 优化：可以减少一个变量
+            let prev: Node | null = null;
+            let cur = head.next;
+            while(cur != null){
+                let next : Node | null = cur.next; // 记录正常顺序中的下一结点
+                cur.next = prev;    // 改变当前结点的next指向
+                prev = cur; // 代表"上一个结点"的指针 指向 当前结点 (其上cur就是"当前结点的指针")
+                cur = next; // "当前结点的指针" 指向 正常顺序中的下一结点
+            }
+        }
+
+        // 创建长度为n的链表
+        create(num: number) {
+            for (let i = 1; i <= num; i++) {
+                this.append('' + i);
             }
         }
     }
 
     function palindrome_test() {
         let a = new Linked_list_palindrome(5);
+        a.create(6);
         a.findcenter();
+        a.display();
+        a.reverse();
+        a.display();
     }
-
+    palindrome_test();
 }();
