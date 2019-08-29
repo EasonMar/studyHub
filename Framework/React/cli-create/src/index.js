dac9f6372433084798389806ad023541c390016e
EasonMar <12054648@qq.com>
2019-08-29 11:00:16 +0800

    react study project： 1. react-router（v4 = react-router-dom）

---
+++ Framework/React/cli-create/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import App from './AuthRedirect';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
