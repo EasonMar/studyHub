95a2a0f6a218746a2031b9a0c8a37226349c1c51
EasonMar <12054648@qq.com>
2021-05-26 23:00:23 +0800

    feat: Exclude、Omit

--- Typescript/Advanced/demo.ts
+++ Typescript/Advanced/demo.ts
@@ -1,12 +1,66 @@
+// 学习 Record 引发的一系列思考...
+// - keyof any
+// - [P in number]: string
+// - - in 关键字
+// - - - 勿与 keyof 混淆
+// - - 索引类型
+// - extends
+// - - 类型兼容
+
 interface EmployeeType {
     id: number,
     fullname: string,
     role: string
 }
 
+type keyOfEmployeeType = keyof EmployeeType
+// type fullname = Pick<EmployeeType, 'a'> // Type '"a"' does not satisfy the constraint '"id" | "fullname" | "role"'
+
 let employees: Record<number, EmployeeType> = {
     0: { id: 1, fullname: 'John', role: 'Designer' }
 }
 
+
 // 如何理解 [P in number]: string
+// type t = Record<number, string> // { [name: number]: string } --- ? 不理解为什么是这样...
+// 解析: in 关键字是 对「枚举类型」进行遍历, 这里 P in number, 相当于 只有一个元素的素组的遍历... 自然就得到 [name: number]
+// type t = Record<1, string> // { 1: string }
+// type t = Record<"prop", string> // { prop: string }
+// type t = Record<string, string> // { [name: string]: string }
 // type t = Record<number, string> // { [name: number]: string }
+// 了解一下「索引类型」:
+
+
+type AnimalType = 'cat' | 'dog' | 'frog';
+interface AnimalDescription { name: string, icon: string }
+// const AnimalMap: Record<AnimalType, AnimalDescription> = {
+//   cat: { name: '猫', icon: ' '},
+//   dog: { name: '狗', icon: ' ' },
+//   forg: { name: '蛙', icon: ' ' }, // Hey!
+// };
+
+
+type keyofAny = keyof any // string | number | symbol
+type result = AnimalType extends keyofAny ? true : false // 为啥result为true... 真TM不好理解啊... 
+// 可不可以这样理解: 字符串字面量联合类型与string是兼容的
+// 了解一下「类型兼容」:
+
+let myString = 'a'
+type myStringType = typeof myString
+
+
+// ==========================================================================================
+// 学习 Omit 引发的思考
+// - Omit 可认为是「适用于键值对对象」的 Exclude --- Exclude 适用于什么对象
+// - Exclude_Tools<T, K> 返回的是啥...
+// - [P in Exclude_Tools<T, K>] 为啥报错
+type UserO = {
+    id: number;
+    name: string;
+    location: string;
+    registeredAt: Date;
+};
+
+type userExcludeObject = Exclude<UserO, "id" | "registeredAt"> // 没有用... Exclude 适用于联合类型对象
+type userExcludeUnion = Exclude<keyof UserO, "id" | "registeredAt"> // 有用
+
--- Typescript/Advanced/tools.ts
+++ Typescript/Advanced/tools.ts
@@ -33,7 +33,8 @@ type Record_Tools<K, T> = {
 }
 
 // 为什么官方的实现是如下这样？ 跟上面的实现有啥不同 --- 感觉自己审题有问题
-// --- 需要了解[可索引类型]...
+// 解析一、深入了解 in 关键字的作用
+// 解析二、了解一下「索引类型」
 type Record_Tools_Official<K extends keyof any, T> = {
     [P in K]: T;
 };
@@ -57,18 +58,37 @@ type Pick_Tools<T, K extends keyof T> = {
 
 // 正解
 type Exclude_Tools<T, U> = T extends U ? never : T
-// 解析: 深入了解extends的意义... 及其 分配式 特点
+// 解析一、深入了解extends的意义... 及其 分配式 特点
 // https://juejin.cn/post/6844904146877808653
+
+// 解析二、任何类型联合上 never 类型，结果还是原来的类型
 
 // 举一反三
 // Include<T, U> - 在 T 类型中，返回 T 类型和 U 类型的交集
 type Include_Tools<T, U> = T extends U ? T : never
 
+
 // ==========================================================================================
 
-// Omit<T, K> - 去除类型 T 中包含 K 的键值对, 可认为是适用于键值对对象的 Exclude
+// Omit<T, K> - 去除类型 T 中包含 K 的键值对, 可认为是「适用于键值对对象」的 Exclude --- Exclude 适用于什么对象？--- 我的理解是「联合类型对象」
 
-// missed: 
+// missed: 反思 Exclude_Tools的使用场景、Exclude_Tools<T, K> 返回的是啥...
 // type Omit_Tools<T, K> = {
-//     [P in keyof Exclude_Tools<T, K>]: T[P]
+//     [P in keyof Exclude_Tools<T, K>]: T[P] // Type 'P' cannot be used to index type 'T'
 // }
+
+// missed: Exclude_Tools<T, K> 返回的不一定是能作为 key 的类型...
+// type Omit_Tools<T, K> = {
+//     [P in Exclude_Tools<T, K>]: T[P] // Type 'Exclude_Tools<T, K>' is not assignable to type 'string | number | symbol'.
+// }
+
+// 解析一、K extends keyof T: 约束K的值
+// 解析二、Exclude_Tools 适用于联合类型, 所以 Exclude_Tools<keyof T, K> 才能正常返回 T 剔除 K 的剩余 key
+type Omit_Tools<T, K extends keyof T> = {
+    [P in Exclude_Tools<keyof T, K>]: T[P]
+}
+
+// 优化 --- 实际上前面包含了一个 Pick 的逻辑...可以直接用 Pick 来封装以上逻辑
+type Omit_Tools_upgrate<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+
+// ==========================================================================================
